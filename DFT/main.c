1c5b3c702b9e8204a9aab23fe6b58daa4ff22ab2
amjadmajid <amjad.y.majid@gmail.com>
2017-05-18 18:25:05 +0200

    Initial implementation of DFT for IPOS

---
+++ DFT/main.c
@@ -0,0 +1,182 @@
+#include <msp430.h> 
+#include <math.h>
+#include "ipos.h"
+#include <stdint.h>
+
+
+
+
+#if 0
+
+#define SIZE 16
+#define PI2 6.2832
+
+  float x[SIZE];              // discrete-time signal, x
+  float Xre[SIZE], Xim[SIZE]; // DFT of x (real and imaginary parts)
+  float P[SIZE];              // power spectrum of x
+
+  void dft_c() {
+
+    int n, k; // iSIZEdices for time and frequency domains
+
+    // Generate random discrete-time signal x in range (-1,+1)
+    srand(0);
+    for (n = 0; n < SIZE; ++n)
+      x[n] = ((2.0 * rand()) / RAND_MAX) - 1.0;
+
+
+
+    // Calculate DFT of x using brute force
+    for (k = 0; k < SIZE; ++k) {
+      // Real part of X[k]
+      Xre[k] = 0;
+
+      for (n = 0; n < SIZE; ++n)
+        Xre[k] += x[n] * cosf(n * k * PI2 / SIZE);
+
+
+      // Imaginary part of X[k]
+      Xim[k] = 0;
+      for (n = 0; n < SIZE; ++n)
+        Xim[k] -= x[n] * sinf(n * k * PI2 / SIZE);
+
+      // Power at kth frequency bin
+      P[k] = Xre[k] * Xre[k] + Xim[k] * Xim[k];
+    }
+
+
+  }
+#endif
+
+void discTimeSign();
+void dft_outer_loop() ;
+void dft_real();
+void dft_im();
+void  dft_power();
+void dft_end() ;
+static void blinkLed(uint32_t wait );
+static void burn( uint32_t iters);
+
+#define SIZE 16
+#define PI2 6.2832
+
+__v  float x[SIZE];              // discrete-time signal, x
+__v  float Xre[SIZE], Xim[SIZE]; // DFT of x (real and imaginary parts)
+__v  float P[SIZE];              // power spectrum of x
+__v uint8_t n =0, k = 0;
+
+
+
+void discTimeSign()
+{
+    // Generate random discrete-time signal x in range (-1,+1)
+    x[n] = ((2.0 * rand()) / RAND_MAX) - 1.0;
+    n++;
+
+    if(n > SIZE){
+        os_unblock(dft_outer_loop);
+        os_block(discTimeSign);
+    }
+}
+
+// Calculate DFT of x using brute force
+
+void dft_outer_loop() {
+    if(k > SIZE)
+    {
+        os_unblock(dft_end);
+        os_block(dft_outer_loop);
+    }
+    Xre[k] = 0;
+    Xim[k] = 0;
+    n = 0;
+    os_unblock(dft_real);
+    os_block(dft_outer_loop);
+}
+
+void dft_real() {
+
+  Xre[k] += x[n] * cos(n * k * PI2 / SIZE);
+  n++;
+
+  if(n > SIZE){
+      os_unblock(dft_im);
+      os_block(dft_real);
+      n=0;
+  }
+}
+
+void dft_im() {
+    Xim[k] -= x[n] * sin(n * k * PI2 / SIZE);
+    n++;
+    if(n > SIZE){
+        os_unblock(dft_power);
+        os_block(dft_im);
+        n=0;
+    }
+}
+
+void  dft_power(){
+    P[k] = Xre[k] * Xre[k] + Xim[k] * Xim[k];
+    os_unblock(dft_outer_loop);
+    os_block(dft_power);
+    k++;
+
+}
+
+void dft_end() {
+    P3OUT |= BIT5;
+    P3OUT &= ~BIT5;
+
+    blinkLed(2500);
+
+    os_unblock(discTimeSign);
+    os_block(dft_end);
+}
+
+void init()
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+    // Disable the GPIO power-on default high-impedance mode to activate previously configured port settings.
+    PM5CTL0 &= ~LOCKLPM5;       // Lock LPM5.
+    P3DIR |=BIT5;
+}
+
+//delay
+volatile uint32_t work_x;
+static void burn( uint32_t iters)
+{
+    uint32_t iter = iters;
+    while(iter--)
+        work_x++;
+}
+
+
+//blink an led once
+static void blinkLed(uint32_t wait )
+{
+    P4DIR |= BIT0;
+    P4OUT |= BIT0; // lit the LED
+    burn(wait); // wait
+    P4OUT &= ~BIT0; // dim the LED
+    burn(wait); // wait
+}
+
+
+int main(void) {
+    init();
+
+
+       taskId tasks[] = {  {discTimeSign,0},
+                           {dft_outer_loop,1},
+                           {dft_real,1 },
+                           {dft_im, 1},
+                           {dft_power, 1},
+                           {dft_end,1}
+       };
+       //This function should be called only once
+       os_addTasks(6, tasks );
+
+       os_scheduler();
+       return 0;
+}
