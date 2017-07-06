48c48942ed54f3aa5953a8b9b73d172836fdfa3d
amjadmajid <amjad.y.majid@gmail.com>
2017-07-06 16:57:39 +0200

    implement paging, not finished

--- DMA/ipos-gcc-dma/include/dataProtec.h
+++ DMA/ipos-gcc-dma/include/dataProtec.h
@@ -8,8 +8,26 @@
 #ifndef INCLUDE_DATAPROTEC_H_
 #define INCLUDE_DATAPROTEC_H_
 
-void repopulate();
-void wb_firstPhaseCommit();
-void wb_secondPhaseCommit();
+
+uint16_t __is_varInCrntPag(uint16_t * var);
+
+void __sendPagTemp(uint16_t pagTag);
+void __bringPagTemp(uint16_t pagTag);
+void __bringPagROM(uint16_t pagTag);
+void __sendPagROM(uint16_t pagTag);
+void __pagsCommit();
+
+
+// Memory Access interface
+                                         // Shift the var to RAM
+#define WVAR(var)  (*(__typeof__(var)*  ( (unsigned int)&var | BIGEN_RAM)))     = var;
+
+
+                    // check var in current page
+#define RVAR(var)   ( __is_varInCrntPag( &var) )?\
+                    // read the var from the current page
+                    ( *(__typeof__(var)* )( ((unsigned int)&var) | BIGEN_RAM) ):\
+                    // do page swapping and read the var
+                    ( *(__typeof__(var)* )(__pageSwap(&var)+( ((unsigned int)&var) | BIGEN_RAM) ) );
 
 #endif /* INCLUDE_DATAPROTEC_H_ */
