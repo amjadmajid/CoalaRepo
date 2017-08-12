1339c8f71d05131569f2d395285551db2abae728
amjadmajid <amjad.y.majid@gmail.com>
2017-08-12 04:00:54 +0200

    page size, page fault overhead

--- experiments/PagingSizeOverhead/pagefaults.py
+++ experiments/PagingSizeOverhead/pagefaults.py
@@ -43,23 +43,14 @@ apps_names=[
     "sort"
 ]
 
-row_cuc = np.array([1203109, 420241, 380692,147507, 148331, 513967, 805221,1257204])
-row_cuc_norm = row_cuc / np.min(row_cuc)
 
-row_cem = np.array([8549451,5388765,3626039,3814352,4608503, 6421565, 10096537,17589788])
-row_cem_norm = row_cem / np.min(row_cem)
-
-row_bc = np.array([788926, 615698, 638402, 660397,732470, 911575, 1274839, 1961663])
-row_bc_norm = row_bc / np.min(row_bc)
-
-row_sort = np.array( [2937186,2734324,2495083,2317925,1243372,1566328,2219896,3460253])
-row_sort_norm = row_sort / np.min(row_sort)
-
-row_dft = np.array( [28438580,28458614,28511341,28572634,28464076,28075348,28308658,28880930])
-row_dft_norm = row_dft / np.min(row_dft)
-
-
-data_muc = [row_cem_norm, row_cuc_norm, row_dft_norm, row_bc_norm,row_sort_norm]
+data_page = [
+    np.array([8498,7354,5367,5379,5344,5295,5237,5236])/(8498/3),
+    np.array([759, 137, 130,49,39,36,0,0]) /(759/3)  ,
+    np.array([3249,3225,3201,3177,1953,100,0,0])/(3249/3),
+    np.array([602, 0, 0,0,0,0,0,0])/(602/3),
+    np.array([5350,4430,3510,2782,0,0,0,0])/(5350/3) 
+]
 
 
 
@@ -68,8 +59,8 @@ figureSetting()
 
 
 s=0
-for d in data_muc:
-  plt.bar( np.arange(len(d)) + s, d, color='y')
+for d in data_page:
+  plt.bar( np.arange(len(d)) + s, d, color='r')
   s+=8.5 
 
 ax = plt.axes()
@@ -77,7 +68,7 @@ ax.xaxis.set_major_locator(ticker.FixedLocator([4,12.5,21,29.5,38]) )
 ax.xaxis.set_major_formatter(ticker.FixedFormatter(apps_names ) )
 
 
-plt.yticks([0, 2, 4,6,8])  
+plt.yticks([0, 2, 4])  
 
 plt.ylabel('Norm. runtime')
 
@@ -85,3 +76,7 @@ plt.tight_layout()
 f.savefig("../figures/pagSizeOverhead.eps",format="eps", dpi=1200)
 f.savefig("../figures/pagSizeOverhead.pdf",format="pdf", dpi=1200)
 plt.show()
+
+
+
+
