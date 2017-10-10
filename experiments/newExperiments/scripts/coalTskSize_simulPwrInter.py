3b083dc2485a0923f8c6023675fa2c7403255331
amjadmajid <amjad.y.majid@gmail.com>
2017-10-10 16:07:57 +0200

    gather some initial data

--- experiments/newExperiments/scripts/coalTskSize_simulPwrInter.py
+++ experiments/newExperiments/scripts/coalTskSize_simulPwrInter.py
@@ -6,18 +6,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 apps, files = filesFinder('simulatedPwrInter_1500/CoalescedTaskSize/*.txt')
+fifNUm = 0
 
+files = files[0]
 for file in files:
+	print(file)
+	fifNUm+=1
 	data=[]
-	for line in open('../data/user/'+file[0]):
+	for line in open('../data/user/'+file):
+		if len(line) < 2:
+			continue
 		num = int(line, 16)
 		if(num > 64):
 			continue 
 		data.append(num)
 	meanData = [np.mean(data)] * len(data) 
 	
-	plt.figure()
+	plt.figure(fifNUm)
 	plt.plot(data)
 	plt.plot(meanData)
-	
+
 plt.show()
